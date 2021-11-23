80be6e457018f8f776976b62650fcae7516ae747
Romantic <72870719+dpy0912@users.noreply.github.com>
2021-11-23 22:08:36 +0800

    港口点位

--- src/api/service/point.js
+++ src/api/service/point.js
@@ -1,2 +1,9 @@
 // 点位表
 import api from "../../common/interface"
+
+/**
+ * 查询port_point_map_id下的点位图
+ * @param {*} port_point_map_id 
+ * @returns 
+ */
+export const getPointSearch = port_point_map_id => api.get(`/point/search?port_point_map_id=${port_point_map_id}`, {})
