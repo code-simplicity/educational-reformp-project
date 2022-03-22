083a3e03eb88cf44a2a956d6ca9856d1892199af
Romantic <dupyi0912@gmail.com>
2022-03-22 21:03:58 +0800

    fix: 添加成功的状态常量

--- src/utils/Constants.js
+++ src/utils/Constants.js
@@ -3,8 +3,17 @@ import config from "../api/config";
 const {
   api_base_url
 } = config;
-// const baseURL = "http://localhost:5050"
 const baseURL = api_base_url;
+
+const status = {
+  // 成功
+  SUCCESS: 20000,
+  // 失败
+  FAILED: 40000,
+  // api调用过于频繁
+  API_BUSY: 40001
+}
 export default {
   baseURL,
+  status
 };
