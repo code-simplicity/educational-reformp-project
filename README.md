e900d164a55b1d0e0a997dc3a59466523dfacb74
Romantic <dupyi0912@gmail.com>
2022-04-18 20:57:20 +0800

    first: 更改一个新的分支

---
+++ README.md
@@ -0,0 +1,73 @@
+<h1 align="center">educational-reformp-project-毕业设计门户</h1>
+
+# Vue 3 + Vite
+
+## 下载依赖
+
+```shell
+yarn
+```
+
+## 启动服务
+
+```shell
+yarn dev
+```
+
+## 打包
+
+```shell
+yarn build
+```
+
+## 技术说明
+
+- vue3、vue-router4、vuex4、vite
+- axios
+- sass、
+- element-plus1.0.2
+- xgplayer
+- nprogress
+
+## 记录一下这里出现的问题
+
+首先就是登录，这里登录后端是返回一个tokenKey给前端，前端接受到这个tokenKey之后，然后登录成功了之后，将这个tokenKey加入了后面需要用到该接口的请求头中，例如下面这个
+
+```js
+instance.interceptors.request.use((config) => {
+    // 判断token是否存在，这个token是存在redis中的，存在tokenKey，给请求头加上这个值
+    const tokenKey = getToken()
+    console.log("tokenKey", tokenKey)
+    if (tokenKey !== "" && tokenKey !== undefined && tokenKey !== null) {
+        console.log('config', config)
+        config.headers.common['authorization'] = tokenKey
+    }
+    return config;
+}, error => {
+    return new Promise.reject(error)
+})
+```
+
+那么后端是这么解决这个问题的，首先就是后端这块会设置跨域，比如下面这样
+
+```js
+app.use((req, res, next) => {
+  //判断路径
+  if (req.path !== "/" && !req.path.includes(".")) {
+    res.set({
+      "Access-Control-Allow-Credentials": true, //允许后端发送cookie
+      "Access-Control-Allow-Origin": req.headers.origin || "*", //任意域名都可以访问,或者基于我请求头里面的域
+      "Access-Control-Allow-Headers": "Content-Type, Authorization, X-Requested_With", //设置请求头格式和类型
+      "Access-Control-Allow-Methods": "PUT,POST,GET,DELETE,OPTIONS", //允许支持的请求方式
+      "Content-Type": "application/json; charset=utf-8", //默认与允许的文本格式json和编码格式
+    });
+  }
+  if (req.method.toLowerCase() == "options") {
+    res.status(204).send(200)
+  } else {
+    next()
+  }
+});
+```
+
+上面代码中配置了`options`，为什么这样呢，首先就是携带有一种请求叫做`Preflighted Request`（带预检的跨域请求），`Preflighted Request`，在发送真正的请求前，会先发送一个方法为`OPTIONS`的预请求`(Preflighted Request)`，用于试探服务端是否能接受真正的请求。如果`options`获得的回应时拒绝性质的，如`404、403、500`等状态，就会停止`post、get`请求的发出的请求，我这里当时是做了错误的处理，导致一直是产生`cors`。
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,200 @@
+<template>
+	<canvas id="myCanvas" class="myCanvas"></canvas>
+	<canvas id="myAxisGizmoCanvas"></canvas>
+	<canvas id="myNavCubeCanvas"></canvas>
+	<div id="myDatGuiContainer"></div>
+</template>
+<script setup>
+import { onMounted, ref, defineProps, onUnmounted } from "vue";
+import {
+	Viewer,
+	GLTFLoaderPlugin,
+	AxisGizmoPlugin,
+	LocaleService,
+	NavCubePlugin,
+	DirLight,
+	AmbientLight,
+} from "@xeokit/xeokit-sdk";
+import * as dat from "dat.gui";
+const props = defineProps({
+	// 模型的地址
+	glftUrl: String,
+});
+// 放大初始值
+let glftZoom = ref(0);
+
+// 模型初始化方法
+const gltfModelInit = () => {
+	// 首先定义viewr视图,传入dom的id
+	const viewer = new Viewer({
+		canvasId: "myCanvas", // dom的id
+		transparent: true, // 属性值为透明
+		// 语言服务，提供方向
+		localeService: new LocaleService({
+			messages: {
+				en: {
+					// English
+					NavCube: {
+						front: "前",
+						back: "后",
+						top: "上",
+						bottom: "下",
+						left: "左",
+						right: "右",
+					},
+				},
+			},
+			locale: "en",
+		}),
+	});
+	// 默认的语言服务
+	viewer.localeService.locale = "en";
+	// 以视图驱动相机
+	viewer.camera.orbitPitch(0); // 轨道间距为20
+	viewer.camera.orbitYaw(0); // 轨道偏航
+	// 开启sao,环境光遮蔽
+	viewer.scene.sao.enabled = true;
+	viewer.scene.sao.numSamples = 60;
+	viewer.scene.sao.kernelRadius = 170;
+	viewer.camera.project.fov = 45;
+	// 缩放
+	viewer.camera.zoom(5);
+
+	// 点位注解
+	viewer.scene.camera.eye = [-2.37, 18.97, -26.12];
+	viewer.scene.camera.look = [10.97, 5.82, -11.22];
+	viewer.scene.camera.up = [0.36, 0.83, 0.4];
+	// 场景所选材料为填充阿尔法
+	viewer.scene.selectedMaterial.fillAlpha = 0.1;
+
+	// 开启右侧方向控制
+	new NavCubePlugin(viewer, {
+		canvasId: "myNavCubeCanvas",
+		color: "lightblue",
+		visible: true,
+		size: 250,
+		cameraFly: true,
+		cameraFitFOV: 45,
+		cameraFlyDuration: 0.5,
+	});
+
+	const guiParams = new (function () {
+		this["Current Locale"] = viewer.localeService.locale;
+	})();
+
+	const update = function () {
+		viewer.localeService.locale = guiParams["Current Locale"];
+		requestAnimationFrame(update);
+	};
+
+	update();
+	// 数据gui加速
+	const gui = new dat.GUI({ autoPlace: false, width: "100%" });
+
+	const localizationFolder = gui.addFolder("Localization");
+	localizationFolder.add(
+		guiParams,
+		"Current Locale",
+		viewer.localeService.locales
+	);
+	localizationFolder.open();
+
+	const customContainer = document.getElementById("myDatGuiContainer");
+	customContainer.appendChild(gui.domElement);
+
+	window.viewer = viewer;
+
+	// 加载器插件
+	const gltfLoader = new GLTFLoaderPlugin(viewer);
+
+	// 坐标轴
+	new AxisGizmoPlugin(viewer, {
+		canvasId: "myAxisGizmoCanvas",
+	});
+	// 缩放比例值
+	// 设置模型，并且加载模型
+	let model = gltfLoader.load({
+		id: "myModel",
+		src: "https://bugdr-project-1305152720.cos.ap-beijing.myqcloud.com/helicopter.gltf",
+		// 边缘化
+		edges: true,
+		// 表现化
+		performance: true,
+		rotation: [0, 0, 0], // 位置
+		scale: [0.5, 0.5, 0.5], // 缩放
+		position: [glftZoom.value * 100, 0, 0], // 放大
+		pbr: true, // 加载多个模型
+		sao: true, // 加载多个模型
+	});
+
+	// 加载模型
+	model.on("loaded", () => {
+		// 设置元模型
+		// 相机飞行
+		viewer.cameraFlight.flyTo(model);
+		viewer.cameraControl.on("picked", function (e) {
+			console.log(e.entity.id);
+		});
+	});
+	// 模型场景化
+	model = viewer.scene.models["myModel"];
+
+	// 方向光
+	new DirLight(viewer.scene, {
+		dir: [-0.8, -0.6, -0.8],
+		color: [1.0, 1.0, 0.9],
+		intensity: 1.0,
+		space: "world",
+	});
+	// 环境光
+	new AmbientLight(viewer.scene, {
+		color: [0.9, 0.9, 1.0],
+		intensity: 0.8, // 强度
+	});
+	// 销毁模型
+	// gltfModelDestroy(model);
+};
+
+// dom节点生成之后再调用
+onMounted(() => {
+	gltfModelInit();
+});
+
+// 销毁
+onUnmounted(() => {});
+</script>
+<style lang="scss" scoped>
+.myCanvas {
+	position: absolute;
+	z-index: 100;
+	left: 0;
+	top: 0;
+	width: 100%;
+	height: 100%;
+	overflow: auto;
+	background: lightblue;
+	background-image: linear-gradient(lightblue, white);
+}
+#myAxisGizmoCanvas {
+	position: absolute;
+	width: 250px;
+	height: 250px;
+	bottom: 100px;
+	left: 50px;
+	z-index: 200000;
+}
+#myNavCubeCanvas {
+	position: absolute;
+	width: 250px;
+	height: 250px;
+	bottom: 100px;
+	right: 50px;
+	z-index: 200000;
+}
+.myZoom {
+	position: absolute;
+	top: 50px;
+	right: 50px;
+	z-index: 200000;
+}
+</style>
