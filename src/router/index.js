9bf750033b3461e3734efba5d949c0709830d208
peiyi.du <peiyi.du@huilianyi.com>
2025-04-05 19:54:27 +0800

    build(development): 更新 API 地址并修改路由历史模式
    - 更新 .env.production 中的 VITE_SERVICE_BASE_URL 为本地 IP地址
    - 在 src/router/index.js 中将路由历史模式从 createWebHistory 改为 createWebHashHistory

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,9 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import { routes } from './router';
 import store from '../store'; // 导入 Vuex store
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
+	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes,
 });
 
